6970790c960ca934b6c2735c403620e29bc81669
FOFOx-YH <2179300656@qq.com>
2025-12-18 10:41:23 +0800

    update 2025/12/18 周四 10:41:22.88

--- update.bat
+++ update.bat
@@ -1,7 +1,19 @@
 @echo off
+REM 自动提交、推送并部署 MkDocs
+
+set DATETIME=%date% %time%
+
+echo Git add...
 git add .
-git commit -m "update"
+
+echo Git commit...
+git commit -m "update %DATETIME%"
+
+echo Git push...
 git push origin main
+
+echo Deploy MkDocs...
 python -m mkdocs gh-deploy --force
-echo Done
+
+echo Done!
 pause
